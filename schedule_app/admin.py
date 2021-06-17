915532177650934887be180efa6316cfef497a54
Eric Souza <eric.bh18souza@gmail.com>
2021-06-17 20:06:47 -0300

    New filter configs have been added

--- schedule_app/admin.py
+++ schedule_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'event_date', 'created_at')
+    list_filter = ('title', )
+
+
+admin.site.register(Event, EventAdmin)
